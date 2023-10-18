71b01a833608ed6adeb9969af2c66e6066b8858b
Mua <87473662+MuaSevim@users.noreply.github.com>
2023-10-18 18:48:44 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ const App = () => {
               <Route path="/artists/:id" element={<ArtistDetails />} />
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
+             <Route path="/top-categories" element={<TopCategories />}/>
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
